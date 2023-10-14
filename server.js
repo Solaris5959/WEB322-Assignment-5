02f271e194c5ef8983388580c2c435cb51933840
Connor McDonald <connormmcdonald@gmail.com>
2023-10-13 23:00:37 -0400

    Final Commit

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/********************************************************************************
+*  WEB322 – Assignment 03
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Connor McDonald_______ Student ID: 136123221_____ Date: 10/13/2023____
+*
+*  Published URL: https://agreeable-ox-sandals.cyclic.cloud
+*
+********************************************************************************/
+
+
 const legoData = require("./modules/legoSets");
 const express = require('express');
 var path = require('path');
